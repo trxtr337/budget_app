4579d1a28a35d9913f3ba15f0eb820f8124422d3
Vadim Charukhchyan <charukhv@mcmaster.ca>
2025-05-15 17:46:59 -0400

    Added basic validation to form inputs

--- Product/app.py
+++ Product/app.py
@@ -14,6 +14,15 @@ def add():
         amount = request.form['amount']
         category = request.form['category']
         description = request.form['description']
+
+        if not amount or not category or not description:
+            return "All fields are required!", 400
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            return "Amount must be a number", 400
+
         budget_entries.append({
             'amount': amount,
             'category': category,
@@ -21,6 +30,7 @@ def add():
         })
         return redirect('/')
     return render_template('add.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
